5ce8caf71d7d348271d5f34657608e86d7056f28
Nicolas Mendoza <nicomendoza274@gmail.com>
2026-02-23 00:52:59 -0300

    update migrations imports

--- migrations/env.py
+++ migrations/env.py
@@ -9,16 +9,16 @@ from sqlalchemy import engine_from_config, pool
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
 
 # Import our models and settings
-from core.classes.settings import settings
-from core.models.audit import Audit
-from core.models.file import File
-from models.category import Category
-from models.customer import Customer
-from models.product import Product
-from models.role import Role
-from models.user import User
-from models.user_code import UserCode
-from models.user_role import UserRole
+from src.core.classes.settings import settings
+from src.core.models.audit import Audit
+from src.core.models.file import File
+from src.models.category import Category
+from src.models.customer import Customer
+from src.models.product import Product
+from src.models.role import Role
+from src.models.user import User
+from src.models.user_code import UserCode
+from src.models.user_role import UserRole
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
